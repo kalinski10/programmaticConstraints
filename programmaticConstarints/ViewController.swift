00f2e2f83ce79c9dfe7da74e958d953652371da6
kalin balabnov <kalin_dude10@live.com>
2020-01-14 14:23:31 +0000

    beginning

--- programmaticConstarints/ViewController.swift
+++ programmaticConstarints/ViewController.swift
@@ -10,11 +10,108 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let cameraImageView: UIImageView = {
+        
+        let cameraButton = "camera_button.png"
+        let firstImage = UIImage(named: cameraButton)
+        let firstImageView = UIImageView(image: firstImage)
+        firstImageView.contentMode = .scaleAspectFill
+        firstImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return firstImageView
+        
+    }()
+    
+    let firstTextView: UITextView = {
+      
+        let textView = UITextView()
+        let attributedText = NSMutableAttributedString(string: "This is a camera button! :)", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
+        attributedText.append(NSAttributedString(string: "\n\n\nThis buautiful peice of tech is revolutionary in the photography field!", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13), NSAttributedString.Key.foregroundColor: UIColor.gray]))
+        textView.attributedText = attributedText
+        textView.textAlignment = .center
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textView
+        
+    }()
+    
+    private let previousButton: UIButton = {
+       
+        let button = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+//        button.backgroundColor = .red
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+    }()
+    
+    private let nextButton: UIButton = {
+       
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+//        button.backgroundColor = .green
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // here is our entry Point
+        view.addSubview(firstTextView)
+        setupLayout()
+        setupBottomControlsLayoyut()
     }
+    
+    private func setupLayout() {
+       // top image view conatiner constraints
+        let topImageContainerView = UIView()
+        view.addSubview(topImageContainerView)
+        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        topImageContainerView.addSubview(cameraImageView)
+        
+        NSLayoutConstraint.activate([
+        topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5),
+        topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor),
+        
+        // cameraImageView constraints
+        cameraImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor),
+        cameraImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor),
+        cameraImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.6),
+        
+        // text view constraints
+        firstTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor),
+        firstTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+        firstTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+        firstTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            
+        ])
 
-
+    
+    }
+    
+    fileprivate func setupBottomControlsLayoyut() {
+        
+        let bottomControlsStackView = UIStackView(arrangedSubviews: [previousButton, nextButton])
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomControlsStackView.distribution = .fillEqually
+        
+        view.addSubview(bottomControlsStackView)
+        
+        NSLayoutConstraint.activate([
+            
+            // prev button constarints
+            bottomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50),
+            bottomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+        
+    }
+    
 }
-
