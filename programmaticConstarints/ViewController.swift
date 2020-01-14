cd724e33134a606ff61b689212b5428e4694d183
kalin balabnov <kalin_dude10@live.com>
2020-01-14 10:38:41 +0000

    Initial Commit

---
+++ programmaticConstarints/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  programmaticConstarints
+//
+//  Created by Kalin Balabanov on 14/01/2020.
+//  Copyright © 2020 Kalin Balabanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
